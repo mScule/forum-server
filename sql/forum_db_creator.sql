71788d4267a21c8fdb4199678976c97ee4e669e4
mScule <vilhelmniemi@hotmail.com>
2022-04-19 16:44:35 +0300

    Database creation script added, and small bug fix where users and comments were flipped

---
+++ sql/forum_db_creator.sql
@@ -0,0 +1,64 @@
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               10.6.7-MariaDB - mariadb.org binary distribution
+-- Server OS:                    Win64
+-- HeidiSQL Version:             11.3.0.6295
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+-- Dumping database structure for forum_db
+CREATE DATABASE IF NOT EXISTS `forum_db` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `forum_db`;
+
+-- Dumping structure for table forum_db.publications
+CREATE TABLE IF NOT EXISTS `publications` (
+  `publication_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) unsigned NOT NULL,
+  `type` enum('post','comment') NOT NULL,
+  `title` char(255) NOT NULL,
+  `content` text NOT NULL,
+  `private` bit(1) NOT NULL,
+  PRIMARY KEY (`publication_id`),
+  KEY `fk_publications_users` (`user_id`),
+  CONSTRAINT `fk_publications_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table forum_db.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `user_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` char(50) NOT NULL DEFAULT '',
+  `email` varchar(345) NOT NULL,
+  `password` char(255) NOT NULL,
+  `image` mediumblob DEFAULT NULL,
+  `disabled` bit(1) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table forum_db.votes
+CREATE TABLE IF NOT EXISTS `votes` (
+  `publication_id` int(11) unsigned NOT NULL,
+  `user_id` int(11) unsigned NOT NULL,
+  `vote` enum('upvote','downvote') NOT NULL,
+  KEY `fk_votes_user_id` (`user_id`) USING BTREE,
+  KEY `fk_votes_publications` (`publication_id`),
+  CONSTRAINT `fk_votes_publications` FOREIGN KEY (`publication_id`) REFERENCES `publications` (`publication_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_votes_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- Data exporting was unselected.
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
--- src/main.js
+++ src/main.js
@@ -17,10 +17,10 @@ const
     app.post("/login", (req, res) => login(req, res));
 
     // User
-    app.post("/users", (req, res) => comments.post(req, res));
-    app.put("/users", (req, res) => comments.put(req, res));
-    app.delete("/users", (req, res) => comments.delete(req, res));
-    app.get("/users", (req, res) => comments.get(req, res));
+    app.post("/users", (req, res) => users.post(req, res));
+    app.put("/users", (req, res) => users.put(req, res));
+    app.delete("/users", (req, res) => users.delete(req, res));
+    app.get("/users", (req, res) => users.get(req, res));
 
     // Post
     app.post("/posts", (req, res) => posts.post(req, res));
@@ -29,10 +29,10 @@ const
     app.get("/posts", (req, res) => posts.get(req, res));
 
     // Comment
-    app.post("/comments", (req, res) => users.post(req, res));
-    app.put("/comments", (req, res) => users.delete(req, res));
-    app.delete("/comments", (req, res) => users.put(req, res));
-    app.get("/comments", (req, res) => users.get(req, res));
+    app.post("/comments", (req, res) => comments.post(req, res));
+    app.put("/comments", (req, res) => comments.delete(req, res));
+    app.delete("/comments", (req, res) => comments.put(req, res));
+    app.get("/comments", (req, res) => comments.get(req, res));
 
     const server = app.listen(config.server.port, () => {
         const
