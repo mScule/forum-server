4affed01a030f3aee78b8b5667aabd09809c87d9
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-04-25 11:05:08 +0300

    Add more parameters to publications query

--- src/api/publications.js
+++ src/api/publications.js
@@ -28,8 +28,12 @@ module.exports = {
         // Get publication rows with certain column values or leave the column values blank to not take their values into account in the query.
         const statement = `SELECT * FROM publications WHERE publication_id = IF (? = '', publication_id, ?) 
             AND user_id = IF (? = '', user_id, ?) 
-            AND type = IF (? = '', type, ?)`;
-        const values = [req.body.publication_id, req.body.publication_id, req.body.user_id, req.body.user_id, req.body.type, req.body.type];
+            AND type = IF (? = '', type, ?)
+            AND title = IF (? = '', title, ?)
+            AND content = IF (? = '', content, ?)
+            AND private = IF (? = '', private, ?)`;
+        const values = [req.body.publication_id, req.body.publication_id, req.body.user_id, req.body.user_id, req.body.type, req.body.type,
+            req.body.title, req.body.title, req.body.content, req.body.content, req.body.private, req.body.private];
         const result = await db.query(statement, values);
 
         console.log("Publication get: " + JSON.stringify(result))
