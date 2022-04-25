61791b2bfcd5b844af6c27efb44ea96b0c7d1c69
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-04-25 12:42:30 +0300

    Add deleting users

--- src/api/users.js
+++ src/api/users.js
@@ -12,13 +12,17 @@ module.exports = {
     put: (req, res) => {
         res.send("Users put");
     },
-    delete: (req, res) => {
-        res.send("Users delete");
+    delete: async (req, res) => {
+        const statement = "DELETE FROM users WHERE user_id=?";
+        const values = [req.body.user_id];
+        const result = await db.query(statement, values);
+
+        res.send("Users delete: " + result);
     },
     get: async (req, res) => {
         let statementLine = "";
 
-        if(req.body.forum_api_key === ""){
+        if (req.body.forum_api_key === "") {
             statementLine = " IS NULL OR forum_api_key IS NOT NULL"
         } else {
             statementLine = "= ?";
@@ -29,7 +33,7 @@ module.exports = {
             AND name = IF (? = '', name, ?)
             AND email = IF (? = '', email, ?)
             AND disabled = IF (? = '', disabled, ?)
-            AND forum_api_key` + statementLine ;
+            AND forum_api_key` + statementLine;
 
         const values = [req.body.user_id, req.body.user_id, req.body.name, req.body.name, req.body.email, req.body.email,
             req.body.disabled, req.body.disabled, req.body.forum_api_key, req.body.forum_api_key];
