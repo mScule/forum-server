af786e0abadfe20c2a965960a508e3174684e80b
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-04-28 12:09:15 +0300

    Use UUIDs to identify users

--- src/api/login.js
+++ src/api/login.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const db = require("../utils/db");
+const uuid = require("uuid");
 
 module.exports = {
     /*
@@ -8,19 +9,20 @@ module.exports = {
     * Insert the username as "name" and password as "password" in the HTTP request's body in JSON format.
     * */
     login: async (req, res) => {
-        let randomNumber = Math.random().toString();
-        randomNumber = randomNumber.substring(2, randomNumber.length);
+        let uuidV4 = uuid.v4();
 
-        const statement = "UPDATE `forum_db`.`users` SET `forum_api_key`=" + randomNumber + " WHERE `name`=? AND `password`=?";
-        const values = [req.body.name, req.body.password];
+        const statement = "UPDATE forum_db.users SET forum_api_key=? WHERE name=? AND password=?";
+        const values = [uuidV4, req.body.name, req.body.password];
         const result = await db.query(statement, values, res);
 
         if (result === undefined || result === "No result") {
             res.status(401);
-            res.send("Error logging in!");
+            res.send("Login failed. Check your username and password.");
+        } else if (result instanceof Error) {
+            res.send("Login error. " + result);
         } else {
             // set a new cookie on login
-            res.cookie('forum_api_key', randomNumber, {maxAge: 900000, httpOnly: true});
+            res.cookie('forum_api_key', uuidV4, {maxAge: 900000, httpOnly: true});
             console.log('cookie created successfully');
 
             res.status(201);
