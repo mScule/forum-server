3b1d914135578dddadcdaba424b556882d1b1522
Jonathan <44569793+Jonanananas@users.noreply.github.com>
2022-05-03 15:10:13 +0300

    Fix a variable name which was overridden in merge

--- src/api/publications.js
+++ src/api/publications.js
@@ -20,7 +20,7 @@ module.exports = {
 
             const statement = `INSERT INTO forum_db.publications (user_id, type, title, content, private, reply_to_id) 
                 VALUES (?, ?, ?, ?, ?, ?)`;
-            const values = [currentUser, req.body.type, req.body.title, req.body.content, 0, req.body.reply_to_id];
+            const values = [currentUserId, req.body.type, req.body.title, req.body.content, 0, req.body.reply_to_id];
             const result = await db.query(statement, values, res);
 
             clearCache("/publications");
